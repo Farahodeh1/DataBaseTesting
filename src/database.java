0142333f9b703190da1c14d63474f213b08e3ab6
Farahodeh1 <farahodeh400@gmail.com>
2026-01-17 23:06:44 +0300

    we have added the screen shot

--- src/database.java
+++ src/database.java
@@ -1,11 +1,18 @@
+import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.Duration;
+import java.util.Date;
+import java.util.Random;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -16,11 +23,13 @@ public class database {
 	
 	
 	WebDriver driver = new ChromeDriver();
+	Random rand = new Random();
 	Connection con ; 
 	Statement stmt ;
 	ResultSet rs ;
 String firstname;
 String lastname;
+Date timeStamp = new Date();
 	
 	
 	@BeforeTest
@@ -32,9 +41,9 @@ String lastname;
 		
 		
 	}
-	@Test (priority = 1)
+	@Test (priority = 2)
 	public void UpdateTheData () throws SQLException, InterruptedException {
-		String query = "update customers set customerName = 'Ali' where customerNumber = 103";
+		String query = "update customers set customerName = 'Farah Odeh' where customerNumber = 103";
 		stmt = con.createStatement();
 		int rowupdated = stmt.executeUpdate(query);
 		System.out.println(rowupdated);
@@ -43,9 +52,14 @@ String lastname;
 	
 	
 	
-	@Test(priority = 2)
-	public void ReadTheDataInsideTheBrowser() throws SQLException {
-		String query = "select * from customers where customerNumber = 103";
+	@Test(priority = 3)
+	public void ReadTheDataInsideTheBrowser() throws SQLException, IOException {
+		String [] customernumber = {"114","119","121"};
+		
+		int randomcustomernumber = rand.nextInt(customernumber.length); 
+		String theselectcustomernumber = customernumber[randomcustomernumber];
+		String query = "select * from customers where customerNumber = 103" ;
+		
 		stmt = con.createStatement();
 		rs = stmt.executeQuery(query);
 		System.out.println(rs);
@@ -63,13 +77,42 @@ String lastname;
 		lastnamebox.sendKeys(lastname);
 		
 		
+		TakeTheScreenShotPlease();
+		
+		
+		
+	}
+	
+	@Test(priority = 1)
+	public void inserttotable() throws SQLException {
+		String query = "INSERT INTO customers (customerNumber, customerName, contactLastName, contactFirstName, phone, addressLine1, addressLine2, city, state, postalCode, country, salesRepEmployeeNumber, creditLimit) VALUES (103, 'Ali', 'Schmitt', 'Carine', '40.32.2555', '54, rue Royale', NULL, 'Nantes', NULL, '44000', 'France', 1370, 21000.00);\r\n"
+				+ "";
+		stmt = con.createStatement();
+		int rowupdated = stmt.executeUpdate(query);
+		
+		
+		
+	}
+	
+	@Test(priority = 4)
+	public void DeleteQuery() throws SQLException {
+		String query = "delete from customers where customerNumber = 103";
+		stmt = con.createStatement();
+		int rowupdated = stmt.executeUpdate(query);
+		
+		
 		
 	}
 	
 	
+public void TakeTheScreenShotPlease() throws IOException {
+	TakesScreenshot ts = (TakesScreenshot)driver;
+	File screenshotfile = ts.getScreenshotAs(OutputType.FILE);
+	String filename = timeStamp.toString().replace(":", "-");
+	FileUtils.copyFile(screenshotfile, new File("src/screenshot‬/" + filename + ".jbg"));
 	
-	
-
+		
+}
 	
 	
 	
